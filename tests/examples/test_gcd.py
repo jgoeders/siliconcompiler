dc87f0141715980c6a1d3438000e279ddbf649b8
Will <5217539+WRansohoff@users.noreply.github.com>
2023-02-09 15:52:32 -0500

    Oops, I fixed too many warnings. There are only 76 missing pin routes, though - the 77th was caused by a missing XDG_RUNTIME_DIR.

--- tests/examples/test_gcd.py
+++ tests/examples/test_gcd.py
@@ -43,8 +43,8 @@ def test_py(setup_example_test):
     # "Creating fake entries in the LUT"
     assert chip.get('metric', 'warnings', step='cts', index='0') == 2
 
-    # Missing route to pin (x77)
-    assert chip.get('metric', 'warnings', step='route', index='0') == 77
+    # Missing route to pin (x76)
+    assert chip.get('metric', 'warnings', step='route', index='0') == 76
 
     # Missing route to pin (x89)
     assert chip.get('metric', 'warnings', step='dfm', index='0') == 89
